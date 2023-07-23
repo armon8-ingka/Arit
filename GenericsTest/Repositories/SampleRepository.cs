764067c210a113a37b8c8ae2414e08940b30860c
Arit <86007100+armon8-ingka@users.noreply.github.com>
2023-07-24 03:55:31 +0530

    Refactored a bit

--- GenericsTest/Repositories/SampleRepository.cs
+++ GenericsTest/Repositories/SampleRepository.cs
@@ -3,29 +3,30 @@ using GenericTest.Models;
 
 namespace GenericTest.Repositories;
 
-public class SampleRepository<T> : ISubordinateInterface<T> 
+public class SampleRepository<T> : ISubordinateInterface<T>
 where T : class
-{    public IEnumerable<T> List(T t)
-    { 
-      var obj = new object();
+{
+  public IEnumerable<T> List(T t)
+  {
+    var obj = new object();
 
-      if(typeof(T) == typeof(SampleModel))
-      {
-        var sampleModel = (SampleModel)Convert.ChangeType(t, typeof(SampleModel));
-        obj = sampleModel.Id;
-      }
-      else
-      {
-        var sampleModel2 = (SampleModel2)Convert.ChangeType(t, typeof(SampleModel2));
-        obj = sampleModel2.EmployeeId;
-      }
-      
-      Console.WriteLine($"Inside SampleRepository()- {typeof(T)}");
-      Console.WriteLine($"Inside SampleRepository()- Id is {obj}");       
-      return new List<T>();
-    }
-      public void ShowMessage(T t)
+    if (typeof(T) == typeof(SampleModel))
     {
-         Console.WriteLine($"Inside SampleRepository() -> ShowMessage({typeof(T)})"); 
+      var sampleModel = (SampleModel)Convert.ChangeType(t, typeof(SampleModel));
+      obj = sampleModel.Id;
+      Console.WriteLine($"Inside SampleRepository()- Id is {obj}");
     }
+    else
+    {
+      var sampleModel2 = (SampleModel2)Convert.ChangeType(t, typeof(SampleModel2));
+      obj = sampleModel2.EmployeeId;
+      Console.WriteLine($"Inside SampleRepository()- EmployeeId is {obj}");
+    }
+    
+    return new List<T>();
+  }
+  public void ShowMessage(T t)
+  {
+    Console.WriteLine($"Inside SampleRepository() -> ShowMessage({typeof(T)})");
+  }
 }
